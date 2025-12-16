1540e9c0afac85da3f8d9c118daf87f364d0b696
monarch-ian471 <iankatengeza@gmail.com>
2025-12-16 23:08:53 +0200

    Refactor Alert component to use h() for rendering SVG icons and improve icon path management

--- frontend/src/components/ui/Alert.vue
+++ frontend/src/components/ui/Alert.vue
@@ -29,7 +29,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, h } from 'vue';
 import Modal from './Modal.vue';
 import Button from './Button.vue';
 
@@ -87,21 +87,31 @@ const confirmVariant = computed(() => {
 });
 
 const icon = computed(() => {
+  const paths = {
+    info: 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
+    warning: 'M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z',
+    danger: 'M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z',
+    success: 'M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z',
+  };
+
   return {
-    render: () => {
-      const paths = {
-        info: 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
-        warning: 'M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z',
-        danger: 'M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z',
-        success: 'M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z',
-      };
-      
-      return (
-        <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d={paths[props.type]} />
-        </svg>
-      );
-    }
+    render: () => h(
+      'svg',
+      {
+        class: 'h-6 w-6',
+        fill: 'none',
+        stroke: 'currentColor',
+        viewBox: '0 0 24 24',
+      },
+      [
+        h('path', {
+          'stroke-linecap': 'round',
+          'stroke-linejoin': 'round',
+          'stroke-width': '2',
+          d: paths[props.type],
+        })
+      ]
+    )
   };
 });
 
